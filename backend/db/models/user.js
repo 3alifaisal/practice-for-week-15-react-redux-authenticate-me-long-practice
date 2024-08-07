045a3d80b3b15186230e8333c79c59ba0cfe9df5
3alifaisal <alifaisalawada000@gmail.com>
2024-08-07 20:58:15 +0200

    login done

---
+++ backend/db/models/user.js
@@ -0,0 +1,115 @@
+"use strict";
+const { Model } = require("sequelize");
+const bcrypt = require("bcryptjs");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      username: {
+        type: DataTypes,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notNull: {
+            msg: "Username is required",
+          },
+          len: {
+            args: [4, 30],
+            msg: "Username must be between 4 and 30 characters",
+          },
+          isNotEmail(value) {
+            if (/\S+@\S+\.\S+/.test(value)) {
+              throw new Error("Username cannot be an email address");
+            }
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 256],
+          isEmail(value) {
+            if (!/\S+@\S+\.\S+/.test(value)) {
+              throw new Error("An email should be in a valid email format");
+            }
+          },
+        },
+      },
+      hashedPassword: {
+        type: DataTypes.STRING.BINARY,
+        allowNull: false,
+        validate: {
+          len: [60, 60],
+        },
+      },
+    },
+    {
+      defaultScope: {
+        attributes: {
+          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"],
+        },
+      },
+      scopes: {
+        currentUser: {
+          attributes: { exclude: ["hashedPassword"] },
+        },
+        loginUser: {
+          attributes: {},
+        },
+      },
+
+      sequelize,
+      modelName: "User",
+    }
+  );
+
+  User.prototype.toSafeObject = function () {
+    const { id, username, email } = this; // context will be the User instance
+    return { id, username, email };
+  };
+
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+  };
+
+  User.getCurrentUserById = async function (id) {
+    return await User.scope("currentUser").findByPk(id);
+  };
+
+  User.login = async function ({ credential, password }) {
+    const { Op } = require("sequelize");
+    const user = await User.scope("loginUser").findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
+        },
+      },
+    });
+    if (user && user.validatePassword(password)) {
+      return await User.scope("currentUser").findByPk(user.id);
+    }
+  };
+
+  User.signup = async function ({ username, email, password }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword,
+    });
+    return await User.scope("currentUser").findByPk(user.id);
+  };
+
+  return User;
+};
