71286592c0a8dbcd793dc551f92e12ba5c0dc155
3alifaisal <alifaisalawada000@gmail.com>
2024-08-16 21:16:15 +0200

    Requested phases done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -7,29 +7,24 @@ router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
 
-// GET /api/test
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
-
 module.exports = router;
 
 // GET /api/set-token-cookie
 const asyncHandler = require("express-async-handler");
 const { setTokenCookie } = require("../../utils/auth.js");
 const { User } = require("../../db/models");
-router.get(
-  "/set-token-cookie",
-  asyncHandler(async (_req, res) => {
-    const user = await User.findOne({
-      where: {
-        username: "Demo-lition",
-      },
-    });
-    setTokenCookie(res, user);
-    return res.json({ user });
-  })
-);
+// router.get(
+//   "/set-token-cookie",
+//   asyncHandler(async (_req, res) => {
+//     const user = await User.findOne({
+//       where: {
+//         username: "Demo-lition",
+//       },
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user });
+//   })
+// );
 
 // // GET /api/restore-user
 // const { restoreUser } = require("../../utils/auth.js");
